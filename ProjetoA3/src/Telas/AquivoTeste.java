ee2c0003194eec978f6ee379dc55158c283a18c6
Leonardo Viana <leleoviana120901@gmail.com>
2026-05-11 20:26:05 -0300

    solicitacao de mudança

--- ProjetoA3/src/Telas/AquivoTeste.java
+++ ProjetoA3/src/Telas/AquivoTeste.java
@@ -4,6 +4,6 @@ package Telas;
 
 public class AquivoTeste {
     public static void main(String[] args) {
-        System.out.println("Arquivo de teste para pull request");
+        System.out.println("mudei carai");
     }
 }
