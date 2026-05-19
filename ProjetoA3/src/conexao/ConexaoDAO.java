f4dee22c22b3e09d5c32f7e14406e962e59d4304
Matheus Jarrete <matheushjcosta08@gmail.com>
2026-05-19 19:28:20 -0300

    Adicionando método de Buscar Cliente por Id e alguns outros ajustes em outros métodos

--- ProjetoA3/src/conexao/ConexaoDAO.java
+++ ProjetoA3/src/conexao/ConexaoDAO.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import conexao.Cliente_FlowBank;
 
 public class ConexaoDAO {
     private final Connection conexao;
@@ -16,15 +17,13 @@ public class ConexaoDAO {
     public void inserirCliente(Cliente_FlowBank cliente) {
         String sql = "INSERT INTO clientes (nome, email, celular, data_nascimento, senha) VALUES (?,?,?,?,?)";  
         
-        try {
-            PreparedStatement ps = conexao.prepareStatement (sql);
+        try (PreparedStatement ps = conexao.prepareStatement (sql)){
             ps.setString(1, cliente.getNome());
             ps.setString(2, cliente.getEmail());
             ps.setString(3, cliente.getCelular());
             ps.setDate(4, cliente.getData_Nascimento());
             ps.setString(5, cliente.getSenha());
-            ps.execute();
-            ps.close();
+            ps.executeUpdate();
         }
         catch (SQLException exception) {
             JOptionPane.showMessageDialog(null, "Erro ao adicionar usuario");
@@ -35,20 +34,47 @@ public class ConexaoDAO {
     public boolean loginCliente(String email, String senha) {
         String sql = "SELECT * FROM clientes WHERE email = ? AND senha = ?";
         
-        try {
-            PreparedStatement ps = conexao.prepareStatement (sql);
+        try (PreparedStatement ps = conexao.prepareStatement (sql)) {
             ps.setString(1, email);
             ps.setString(2, senha);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                return true;
-            }
-            else {
-                return false;
+            
+            try (ResultSet rs = ps.executeQuery()) {
+                return rs.next();
             }
         }
         catch (SQLException exception) {
-            JOptionPane.showMessageDialog(null, "Usuário não cadastrado!");
+            JOptionPane.showMessageDialog(null, "Erro ao realizar login!");
+            throw new RuntimeException(exception);
+        }
+    }
+    
+    public Cliente_FlowBank BuscarClientePorId(Integer id) {
+        String sql = "SELECT * FROM clientes WHERE id = ?";
+        
+        try (PreparedStatement ps = conexao.prepareStatement(sql)){
+            ps.setInt(1, id);
+            
+            try (ResultSet rs = ps.executeQuery()) {
+            
+                if (rs.next()) {
+                    Cliente_FlowBank cliente = new Cliente_FlowBank();
+                
+                    cliente.setId(rs.getInt("id"));
+                    cliente.setNome(rs.getString("nome"));
+                    cliente.setEmail(rs.getString("email"));
+                    cliente.setData_Nascimento(rs.getDate("data_nascimento"));
+                    cliente.setSenha(rs.getString("senha"));
+            
+                    return cliente;
+                }
+                else {
+                    JOptionPane.showMessageDialog(null, "Não foi possível encontrar o cliente!");
+                    return null;
+                }
+            }
+        }
+        catch (SQLException exception) {
+            JOptionPane.showMessageDialog(null, "Erro ao buscar cliente!");
             throw new RuntimeException(exception);
         }
     }
